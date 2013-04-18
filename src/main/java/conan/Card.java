84a3d22b03c636a95fdd45c10131d9e3eb93cf9d
Conan Cook <cook.conan@gmail.com>
2013-04-18 01:26:20 +0100

    Prevent changes to a card's rank and suit

---
+++ src/main/java/conan/Card.java
@@ -0,0 +1,45 @@
+package conan;
+
+public class Card {
+
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + suit + ", " + rank + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (rank != card.rank) return false;
+        if (suit != card.suit) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit != null ? suit.hashCode() : 0;
+        result = 31 * result + (rank != null ? rank.hashCode() : 0);
+        return result;
+    }
+}
